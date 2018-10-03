bfc66bacf501de0e1c3241d7215c1fa0a4c8b4a0
J Kenneth King <james@agentultra.com>
2018-10-02 23:26:44 -0400

    Modify src/Lib.hs
    Add a TVar to wrap our AppState and allow scotty action handlers to mutate it.
    
    Added a post action handler to demonstrate how to add users in addition to reading the state.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Lib where
 
+import Control.Concurrent.STM
+import Control.Monad.Reader
 import Data.Aeson hiding (json)
+import Data.Default.Class
+import Data.Text.Lazy hiding (find)
 import Network.HTTP.Types.Status
-import Protolude hiding (get)
-import Web.Scotty
+import Protolude hiding (get, Text)
+import Web.Scotty.Trans
 
 import Data.Monoid (mconcat)
 
@@ -19,22 +24,73 @@ data User =
   }
   deriving (Eq, Generic, Show)
 
+data CreateUser =
+  CreateUser
+  { createUserFirstName :: Text
+  , createUserLastName  :: Text
+  , createUserEmail     :: Email
+  }
+  deriving (Eq, Generic, Show)
+
 instance ToJSON UserId
+instance FromJSON UserId
 instance ToJSON Email
+instance FromJSON Email
 instance ToJSON User
+instance FromJSON User
+instance FromJSON CreateUser
 
-bob = User (UserId 1) "Bob" "Belcher" (Email "bob@burgers.com")
+data AppState = AppState { users :: [User], serial :: Int }
 
-linda = User (UserId 2) "Linda" "Belcher" (Email "linda@burgers.com")
+instance Default AppState where
+  def = AppState [] 0
 
-users :: [User]
-users = [bob, linda]
+newtype WebAction a = WebAction { runWebAction :: ReaderT (TVar AppState) IO a }
+  deriving (Applicative, Functor, Monad, MonadIO, MonadReader (TVar AppState))
 
-run = scotty 3000 $ do
+webAction :: MonadTrans t => WebAction a -> t WebAction a
+webAction = lift
+
+getState :: (AppState -> b) -> WebAction b
+getState f = ask >>= liftIO . readTVarIO >>= return . f
+
+modifyState :: (AppState -> AppState) -> WebAction ()
+modifyState f = ask >>= liftIO . atomically . flip modifyTVar' f
+
+addUser :: Text -> Text -> Email -> AppState -> AppState
+addUser firstName lastName email state =
+  let nextId = (serial state) + 1
+      user = User (UserId nextId) firstName lastName email
+  in
+    state { users = (users state) ++ [user], serial = nextId }
+
+run :: IO ()
+run = do
+  sync <- newTVarIO def
+  let runActionToIO m = runReaderT (runWebAction m) sync
+
+  scottyT 3000 runActionToIO app
+
+app :: ScottyT Text WebAction ()
+app = do
   get "/users" $ do
-    json $ users
+    users' <- webAction $ getState users
+    json $ users'
   get "/users/:id" $ do
     uid <- param "id"
-    case (find (\user -> userId user == UserId uid) users) of
-      Nothing -> status status404
+    users' <- webAction $ getState users
+    case (find (\user -> userId user == UserId uid) users') of
+      Nothing   -> status status404
       Just user -> json user
+  post "/users" $ do
+    b <- body
+    let decodedUser :: Maybe CreateUser = decode b
+    case decodedUser of
+      Nothing -> status status406
+      Just user -> do
+        webAction $ modifyState $
+          addUser
+          (createUserFirstName user)
+          (createUserLastName user)
+          (createUserEmail user)
+        redirect "/users"
