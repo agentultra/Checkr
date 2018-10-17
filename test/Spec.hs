e80d4611a6c4ccc2e0dc790051ceed5d30ce9204
James King <james@agentultra.com>
2018-10-16 22:58:10 -0400

    Modify test/Spec.hs
    Add a pretty useless test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,24 @@
+import Lib
+
+import Protolude
+import Test.Hspec
+
+newtype TestM a = TestM (Identity a)
+  deriving (Functor, Applicative, Monad)
+
+unTestM :: TestM a -> a
+unTestM (TestM (Identity x)) = x
+
+instance MonadDB TestM User where
+  getEntity id =
+    return $ Just $ User (UserId 1) "Bob" "Belcher" (Email "bob@burgers.com")
+  getEntities = return []
+  insertEntity user = return 2
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "MonadDB" $ do
+    context "User" $ do
+      it "returns an empty list when there are no users to fetch" $ do
+        let users' = unTestM getEntities
+        users' `shouldBe` []
